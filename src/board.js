977885b4ae6fbabcebe7a780ae6503da5c73ce15
Gulzhan <gulzhan0404@gmail.com>
2022-07-17 20:31:10 +0600

    last changes

---
+++ src/board.js
@@ -0,0 +1,98 @@
+import Cell from './cell.js';
+
+
+export default class Board {
+    constructor() {
+        this.widthBoard = 4;
+        this.squares = [];
+        this.wrapper = document.querySelector('.grid');
+    }
+    init() {
+        const fragment = document.createDocumentFragment();
+        for (let i = 0; i < this.widthBoard * this.widthBoard; i++) {
+            const square = new Cell();
+            fragment.appendChild(square.getNewElement());
+            this.squares.push(square);
+        }
+        this.wrapper.appendChild(fragment);
+    }
+    generateNewCell() {
+        const randomNumber = Math.floor(Math.random() * this.squares.length);
+
+        if (this.squares[randomNumber].getValue() === '') {
+            this.squares[randomNumber].setValue(2);
+        } else {
+            this.generateNewCell();
+        }
+    }
+    movingColumn(direction) {
+        for (let i = 0; i < this.widthBoard; i++) {
+            this.fillColumn(i, direction === 'up');
+        }
+    }
+    movingRow(direction) {
+        for (let i = 0; i < this.widthBoard * this.widthBoard; i++) {
+            if (i % 4 === 0) {
+                this.fillRow(i, direction === 'left');
+            }
+        }
+    }
+    fillColumn(indexColumn, isUp) {
+        const column = [];
+    
+        for (let i = 0; i < this.widthBoard; i++) {
+            column.push(this.squares[indexColumn + this.widthBoard * i].getValue());
+        }
+        const filteredColumn = column.filter((num) => num);
+        const emptyCellInColumnSize = this.widthBoard - filteredColumn.length;
+        const newColumn = this.makeNewSequence(filteredColumn, emptyCellInColumnSize, isUp);
+        newColumn.forEach((value, i) => {
+            this.squares[indexColumn + (this.widthBoard * i)].setValue(value);
+        });
+    }
+    fillRow(rowIndex, isLeft) {
+        const row = [];
+    
+        for (let i = 0; i < this.widthBoard; i++) {
+            row.push(this.squares[rowIndex + i].getValue());
+        }
+    
+        const filteredRow = row.filter((num) => num);
+        const emptyCellInRowSize = this.widthBoard - filteredRow.length;
+    
+        const newRow = this.makeNewSequence(filteredRow, emptyCellInRowSize, isLeft);
+    
+        newRow.forEach((value, i) => {
+            this.squares[rowIndex + i].setValue(value);
+        });
+    }
+    makeNewSequence(numbers, emptySequensSize, isReverse) {
+        const emptySequence = Array(emptySequensSize).fill('');
+    
+        return isReverse ? numbers.concat(emptySequence) : emptySequence.concat(numbers);
+    }
+    combineColumn() {
+        for (let i = 15; i >= this.widthBoard; i--) {
+            if ((this.squares[i].getValue() === this.squares[i - this.widthBoard].getValue()) && this.squares[i].getValue() !== '') {
+                const combinedTotal = parseInt(this.squares[i].getValue()) + parseInt(this.squares[i - this.widthBoard].getValue());
+                
+                this.squares[i].setValue(combinedTotal);
+                this.squares[i - this.widthBoard].setValue('');
+
+                this.scoreDisplay = this.squares[i].getValue() + combinedTotal;
+            }
+        }
+    }
+    combineRow() {
+        for (let i = 15; i > 1; i--) {
+            if ((this.squares[i].getValue() === this.squares[i - 1].getValue()) && this.squares[i].getValue() !== '' && i % 4 !== 0) {
+                const combinedTotal = parseInt(this.squares[i].getValue()) + parseInt(this.squares[i - 1].getValue());
+                
+                this.squares[i].setValue(combinedTotal);
+                this.squares[i - 1].setValue('');
+
+                this.scoreDisplay = this.squares[i].getValue() + combinedTotal;
+            }
+        }
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,55 @@
+import Board from './board.js';
+
+export const scoreDisplay = document.querySelector('.score');
+export const resultDisplay = document.querySelector('.result');
+
+// GameManager отвечает за общую игровую логику, начать игру, проследить не окончена ли игра, есть ли победитель и подобная логика
+class GameManager {
+    constructor() {
+        this.score = 0;
+        this.isGameOver = false;
+        this.board = null;
+    }
+    init() {
+        this.board = new Board();
+        this.board.init();
+        this.board.generateNewCell();
+        this.board.generateNewCell();
+        document.addEventListener('keyup', this.clickControl.bind(this));
+    }
+    clickControl(event) {
+        if (event.key === 'ArrowUp') {
+            this.board.movingColumn('up');
+            this.board.combineColumn();
+            this.board.generateNewCell();
+        } else if (event.key === 'ArrowDown') {
+            this.board.movingColumn('down');
+            this.board.combineColumn();
+            this.board.generateNewCell();
+        } else if (event.key === 'ArrowRight') {
+            this.board.movingRow('right');
+            this.board.combineRow();
+            this.board.generateNewCell();
+        } else if (event.key === 'ArrowLeft') {
+            this.board.movingRow('left');
+            this.board.combineRow();
+            this.board.generateNewCell();
+        }
+    }
+    checkForGameOver() {
+        const zeros = 0;
+        for (let i = 0; i < this.squares.length; i++) {
+            if (this.squares[i].getValue() == 8) {
+                alert('You win!');
+                document.removeEventListener('keyup', this.clickControl.bind(this));
+            }
+            if (zeros === 0) {
+                resultDisplay.innerHTML = 'You lose!';
+                document.removeEventListener('keyup', this.clickControl.bind(this));
+            }
+        }
+    }
+}
+
+const gameStart = new GameManager();
+gameStart.init();
